f91bef53f6d56bd4d13db323f9177635db06b9c3
Mae Zhao <maezhao27@gmail.com>
2019-06-14 17:54:26 -0400

    graph

--- SW20190530_Ver3/OpticalSwitchControlSequence.xaml.cs
+++ SW20190530_Ver3/OpticalSwitchControlSequence.xaml.cs
@@ -25,20 +25,40 @@ namespace SW20190530_Ver3
     /// </summary>
     public partial class OpticalSwitchControlSequence : Window
     {
-        ComboBox port, group, type;
+        ComboBox port;
+        String type;
         Boolean offline;
-
         public OpticalSwitchControlSequence(MainWindow input)
         {
             InitializeComponent();
             //Presets passed from MainWindow
             port = input.port;
-            group = input.group;
-            type = input.type;
+            type = input.type.Text;
+            type = type.Replace(" ", String.Empty);
+            type = type.Substring(0,type.IndexOf('('));
+            Console.WriteLine("type :" + type);
             offline = input.offlineButton.IsChecked.Value;
+            int o = System.Convert.ToInt32(type.Substring(type.IndexOf("X")).Replace("X", String.Empty));
+            Console.WriteLine(o);
+            
+            int i = System.Convert.ToInt32(type.Substring(2, type.IndexOf("X") - 1).Replace("X", String.Empty));
+            Console.WriteLine(i);
+            SwitchGrid s = new SwitchGrid();
+            Grid grid = s.Load_Grid(4, 2, 3);
+            Grid.SetRow(grid, 1);
+            Main.Children.Add(grid);
+            
+            
+            //TODO different load presets when not online
+            //TODO do soemthing with the port
+        }
 
-            //TODO different load presets depending on offline
-                        
+        //Load path to switch logic
+        //TODO: move the private fields to just the arguements, get rid of used fields
+        private void Load_PTH()
+        {
+            String PTH = File.ReadAllText("Logic_PTH/" + type +".PTH");
+            
         }
         
         //Exit Button
@@ -51,7 +71,12 @@ namespace SW20190530_Ver3
             
                 this.DragMove();
         }
-        
+
+
+
+
+
+
         //Read test components from file
         private void File_Open_Click (object sender, RoutedEventArgs e)
         {
@@ -62,31 +87,52 @@ namespace SW20190530_Ver3
             }
         }
 
+        //Load saved test file NOT done VERY buggy
         private void Load_Saved_Test (String config)
         {
             StringReader test = new StringReader(config);
             //Loading Test Components
             //TODO: remove console writelines
-            int totalSteps = System.Convert.ToInt32(Read_Value(test, '='));
+            int totalSteps = System.Convert.ToInt32(Find_Value(test, '='));
             Console.WriteLine("totalSteps: " + totalSteps);
 
             String title = "";
             if (test.Peek() == 'T')
             {
-                title = Read_Value(test, '=');
+                title = Find_Value(test, '=');
             }
             Console.WriteLine("title: " + title);
 
-            String boardConfig = Read_Value(test, '=');
+            String boardConfig = Find_Value(test, '=');
             Console.WriteLine("boardConfig: " + boardConfig);
 
             ArrayList stepAr = Read_Step_Array(test, totalSteps);
-        
+
+            //IO.DataContext = new SwitchGrid(totalSteps, type, title, stepAr);
 
         }
         
+        //Only follows specific step test file format
+        private ArrayList Read_Step_Array(StringReader test, int totalSteps)
+        {
+            ArrayList stepAr = new ArrayList();
+            int i = 0;
+            for (i = 0; test.Peek() != -1; i++)
+            {
+                SwitchStep step = new SwitchStep(System.Convert.ToDouble(Find_Value(test, '=')), System.Convert.ToInt32(Find_Value(test, '=')));
+                Console.WriteLine(i + " time: " + step.Time1);
+                Console.WriteLine(i + " index: " + step.Index1);
+                stepAr.Add(step);
+            }
+            if (i != totalSteps)
+            {
+                MessageBox.Show("Please fix TotalSteps in Test file");
+            }
+            return stepAr;
+        }
+
         //Returns the rest of the line skipping everything before (and including) the stop
-        private string Read_Value(StringReader test, char stop)
+        private string Find_Value(StringReader test, char stop)
         {
 
             while (test.Peek() != stop && test.Peek() != -1)
@@ -98,28 +144,8 @@ namespace SW20190530_Ver3
                 test.Read();
             }
 
-            return test.ReadLine();            
+            return test.ReadLine();
         }
-
-        //Only follows specific test file format
-        private ArrayList Read_Step_Array(StringReader test, int totalSteps)
-        {
-            ArrayList stepAr = new ArrayList();
-            int i = 0;
-            for (i = 0; test.Peek() != -1; i++)
-            {
-                SwitchStep step = new SwitchStep(System.Convert.ToDouble(Read_Value(test, '=')), System.Convert.ToInt32(Read_Value(test, '=')));
-                Console.WriteLine(i + " time: " + step.Time1);
-                Console.WriteLine(i + " index: " + step.Index1);
-                stepAr.Add(step);
-            }
-            if (i != totalSteps)
-            {
-                MessageBox.Show("Please fix TotalSteps in Test file");
-            }
-            return stepAr;
-        }
-        
-        //Generates DataGrid (collection of table(s))
+       
     }
 }
--- SW20190530_Ver3/SwitchTable.cs
+++ SW20190530_Ver3/SwitchTable.cs
@@ -5,39 +5,135 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SW20190530_Ver3
 {
-    class SwitchTable
+    class SwitchGrid
     {
-        int totalSteps, input, output;
-        string Ttitle, boardConfig;
+        int totalSteps, output, input;
+        string Ttitle;
         ArrayList stepArray;
+        DataTable dt;
 
-        public SwitchTable(int tSteps, int inp, int outp, string tableTitle, string bConfig, ArrayList stepA)
-        { 
-            totalSteps = tSteps;
-            input = inp;
-            output = outp;
-            Ttitle = tableTitle;
-            boardConfig = bConfig;
-            stepArray = stepA;
+        public SwitchGrid()
+        {
+           //TODO used for user defined tests
         }
 
-        public DataTable Create_Switch_Table()
+        public SwitchGrid(int tSteps, string type, string tableTitle, ArrayList stepA)
+        { 
+            totalSteps = tSteps;
+            output = System.Convert.ToInt32(type.Substring(type.IndexOf("X")).Replace("X", String.Empty));
+            input = System.Convert.ToInt32(type.Substring(2, type.IndexOf("X") - 1).Replace("X", String.Empty));
+            Ttitle = tableTitle;
+            stepArray = stepA;
+
+        }
+        //Load default data grid DataGrid TURN INTO CLASS
+        public Grid Load_Grid(int outP, int inP, int rowSteps)
         {
-            DataTable dt = new DataTable(Ttitle);
-            DataRow steps;
-            //Rows
-            steps = dt.NewRow();
+            Grid switchGrid = new Grid();
+            //switchGrid.Width = (double)GridUnitType.Star;
+            //switchGrid.Height = (double)GridUnitType.Star;
+            switchGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            switchGrid.VerticalAlignment = VerticalAlignment.Top;
+            switchGrid.ShowGridLines = true;
+            switchGrid.Background = Brushes.White;
+            RowDefinition s = new RowDefinition();
+            //step.Height = new GridLength(0, GridUnitType.Auto);
+            switchGrid.RowDefinitions.Add(s);
+            if (outP == 1)
+            {
+                for (int i = 1; i < inP; i++)
+                {
+                    ColumnDefinition sPath = new ColumnDefinition();
+                    //sPath.Width = new GridLength(0, GridUnitType.Auto);
+                    switchGrid.ColumnDefinitions.Add(sPath);
+                }
+                for (int i = 1; i < rowSteps; i++)
+                {
+                    RowDefinition step = new RowDefinition();
+                    //step.Height = new GridLength(0, GridUnitType.Auto);
+                    switchGrid.RowDefinitions.Add(step);
+                }
+            }
+            else
+            {
+                //General grouping lables for multiple inputs
+                for (int sw = 1; sw <= outP; sw++) //for each switch, sw value should not be used to represent a switch
+                {
+                    Grid oSwitch = Load_Grid(1, inP, rowSteps);
+                    Grid.SetColumn(oSwitch, sw-1);
+                    Grid.SetRow(oSwitch, 1);
+                    switchGrid.Children.Add(oSwitch);
+
+                    ColumnDefinition lable = new ColumnDefinition();
+                    lable.Width = new GridLength(0, GridUnitType.Auto);
+                    switchGrid.ColumnDefinitions.Add(lable);
+
+                    TextBlock group = new TextBlock();
+                    group.Text = "Output " + sw;
+                    group.FontSize = 12;
+                    group.FontWeight = FontWeights.Bold;
+                    Grid.SetColumn(group, sw - 1);
+                    switchGrid.Children.Add(group);
+                }
+            }
+            return switchGrid;
+        }
 
 
-            for(int i=0; i <totalSteps; i++)
+        //Load default data grid DataGrid
+        private void Load_Table_Headers()
+        {
+            
+            for (int sw = 1; 1 < output; output++) //for each switch, sw value should not be used to represent a switch
             {
                 
+
             }
-            
-            
+        } 
+
+        //ONLY when loading a Table
+        public DataTable Create_Switch_Table()
+        {
+
+            DataColumn lable = new DataColumn();
+            lable.ColumnName = "Step(s)";
+            lable.DataType = Type.GetType("System.Int32");
+            lable.AutoIncrement = true;
+            dt.Columns.Add(lable);
+
+            //Columns
+            for(int i = 0; i<output; i++)
+            {
+                DataColumn inOut = new DataColumn();
+                inOut.DataType = Type.GetType("System.Boolean");
+                inOut.ColumnName = input + "-" + output;
+                inOut.AutoIncrement = true;
+                dt.Columns.Add(inOut);
+            }
+
+            //Rows
+            DataRow steps;
+            for (int i=1; i <=totalSteps; i++)
+            {
+                steps = dt.NewRow();
+                steps["Step(s)"] = i;
+
+            }
+
+            return dt;
+        }
+
+        public void Add_Step()
+        {
+            DataRow steps;
+            steps = dt.NewRow();
+            steps["Step(s)"] = 1;
         }
     }
 }
