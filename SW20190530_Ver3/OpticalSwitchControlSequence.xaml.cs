0c91f3e0a0772270aa663836577cddaa29fc41ce
Mae Zhao <maezhao27@gmail.com>
2019-06-17 18:31:52 -0400

    df

--- SW20190530_Ver3/OpticalSwitchControlSequence.xaml.cs
+++ SW20190530_Ver3/OpticalSwitchControlSequence.xaml.cs
@@ -35,32 +35,79 @@ namespace SW20190530_Ver3
             port = input.port;
             type = input.type.Text;
             type = type.Replace(" ", String.Empty);
-            type = type.Substring(0,type.IndexOf('('));
+            type = type.Substring(0, type.IndexOf('('));
             Console.WriteLine("type :" + type);
             offline = input.offlineButton.IsChecked.Value;
             int o = System.Convert.ToInt32(type.Substring(type.IndexOf("X")).Replace("X", String.Empty));
             Console.WriteLine(o);
-            
+
             int i = System.Convert.ToInt32(type.Substring(2, type.IndexOf("X") - 1).Replace("X", String.Empty));
             Console.WriteLine(i);
-            SwitchGrid s = new SwitchGrid();
-            Grid grid = s.Load_Grid(4, 2, 3);
+            SwitchGrid s = new SwitchGrid(this);
+            Grid grid = Load_Grid(o, i, 5, 0);
             Grid.SetRow(grid, 1);
+            Grid.SetColumnSpan(grid, 2);
+            grid.Margin = new Thickness(10, 10, 10, 10);
             Main.Children.Add(grid);
-            
-            
-            //TODO different load presets when not online
+
+            //TODO load presets
             //TODO do soemthing with the port
         }
 
-        //Load path to switch logic
-        //TODO: move the private fields to just the arguements, get rid of used fields
-        private void Load_PTH()
+        public Grid Load_Grid(int outP, int inP, int rowSteps, int currOut)
         {
-            String PTH = File.ReadAllText("Logic_PTH/" + type +".PTH");
+            Grid switchGrid = new Grid();
+
+            switchGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            switchGrid.VerticalAlignment = VerticalAlignment.Top;
+            switchGrid.ShowGridLines = true;
+            switchGrid.Background = Brushes.White;
+            GridLength WxH = new GridLength(2, GridUnitType.Star);
+
+            ColumnDefinition stepNum = new ColumnDefinition();
+            //stepNum.Name = "stepNum";
+            stepNum.Width = WxH;
+            ColumnDefinition runTime = new ColumnDefinition();
+            runTime.Width = WxH;
+            switchGrid.ColumnDefinitions.Add(stepNum);
+            switchGrid.ColumnDefinitions.Add(runTime);
+
+            RowDefinition chanNum = new RowDefinition();
+            chanNum.Height = WxH;
+            RowDefinition outNum = new RowDefinition();
+            outNum.Height = WxH;
+            switchGrid.RowDefinitions.Add(chanNum);
+            switchGrid.RowDefinitions.Add(outNum);
+
+            //load steps & runtimes
+            for(int s = 1; s <= rowSteps; s++)
+            {
+                RowDefinition nRow = new RowDefinition();
+                nRow.Height = WxH;
+                
+            }
             
+
         }
-        
+        public Graph AddStep(Graph)
+
+
+
+        //Set button does not work
+        private static void SetButton(Grid grid, int row, int column, bool val)
+        {
+            foreach (Button c in grid.Children)
+            {
+                if (Grid.GetRow(c) == row
+                      &&
+                   Grid.GetColumn(c) == column)
+                {
+                    c.Content = val;
+                    break;
+                }
+            }
+        }
+
         //Exit Button
         private void Button_Click_Exit(object sender, RoutedEventArgs e) => Application.Current.Shutdown(99);
         //Min Button
@@ -68,27 +115,29 @@ namespace SW20190530_Ver3
         //Draggable
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            
-                this.DragMove();
+            this.DragMove();
         }
 
 
-
-
-
-
+        //FOLLOWING IS INCOMPLETE: Contains code related to loading and reading existing test cases
         //Read test components from file
-        private void File_Open_Click (object sender, RoutedEventArgs e)
+        private void File_Open_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
-            { 
+            {
                 Load_Saved_Test(File.ReadAllText(openFileDialog.FileName));
             }
         }
+        //Load path to switch logic
+        //TODO: move the private fields to just the arguements, get rid of used fields
+        private void Load_PTH()
+        {
+            String PTH = File.ReadAllText("Logic_PTH/" + type + ".PTH");
 
+        }
         //Load saved test file NOT done VERY buggy
-        private void Load_Saved_Test (String config)
+        private void Load_Saved_Test(String config)
         {
             StringReader test = new StringReader(config);
             //Loading Test Components
@@ -111,7 +160,7 @@ namespace SW20190530_Ver3
             //IO.DataContext = new SwitchGrid(totalSteps, type, title, stepAr);
 
         }
-        
+
         //Only follows specific step test file format
         private ArrayList Read_Step_Array(StringReader test, int totalSteps)
         {
@@ -146,6 +195,8 @@ namespace SW20190530_Ver3
 
             return test.ReadLine();
         }
-       
+
     }
+
+    
 }
--- SW20190530_Ver3/SwitchTable.cs
+++ SW20190530_Ver3/SwitchTable.cs
@@ -13,14 +13,16 @@ namespace SW20190530_Ver3
 {
     class SwitchGrid
     {
+        Window call;
         int totalSteps, output, input;
         string Ttitle;
         ArrayList stepArray;
         DataTable dt;
 
-        public SwitchGrid()
+        public SwitchGrid(Window caller)
         {
-           //TODO used for user defined tests
+            call = caller;
+            //TODO used for user defined tests
         }
 
         public SwitchGrid(int tSteps, string type, string tableTitle, ArrayList stepA)
@@ -32,58 +34,129 @@ namespace SW20190530_Ver3
             stepArray = stepA;
 
         }
-        //Load default data grid DataGrid TURN INTO CLASS
-        public Grid Load_Grid(int outP, int inP, int rowSteps)
+        /// <summary>
+        /// Loads a switch grid:
+        /// <para>outP is the # of output channels </para>
+        /// inP is the # of input channels 
+        /// <para/> rowSteps  is the # of steps 
+        /// <para/>currOut is the current output channel, set to 0 when initiating a new switch grid
+        /// </summary>
+        public Grid Load_Grid(int outP, int inP, int rowSteps, int currOut)
         {
             Grid switchGrid = new Grid();
-            //switchGrid.Width = (double)GridUnitType.Star;
-            //switchGrid.Height = (double)GridUnitType.Star;
+
             switchGrid.HorizontalAlignment = HorizontalAlignment.Left;
             switchGrid.VerticalAlignment = VerticalAlignment.Top;
             switchGrid.ShowGridLines = true;
             switchGrid.Background = Brushes.White;
-            RowDefinition s = new RowDefinition();
-            //step.Height = new GridLength(0, GridUnitType.Auto);
-            switchGrid.RowDefinitions.Add(s);
-            if (outP == 1)
+            //When current output is not 0
+            if (currOut != 0)
             {
-                for (int i = 1; i < inP; i++)
+                RowDefinition outLable = new RowDefinition();
+                switchGrid.RowDefinitions.Add(outLable);
+                outLable.Height = new GridLength(0, GridUnitType.Auto);
+                //Outputs for each Channel
+                for (int i = 1; i <= inP; i++)
                 {
                     ColumnDefinition sPath = new ColumnDefinition();
-                    //sPath.Width = new GridLength(0, GridUnitType.Auto);
+                    //sPath.Width = new GridLength(40, GridUnitType.Auto);
+                    
+                        TextBlock outNum = new TextBlock();
+                        outNum.FontSize = 12;
+                        outNum.Text = "Out " + i;
+                        Grid.SetRow(outNum, 0);
+                        Grid.SetColumn(outNum, i - 1);
+                        switchGrid.Children.Add(outNum);
+                    
                     switchGrid.ColumnDefinitions.Add(sPath);
-                }
-                for (int i = 1; i < rowSteps; i++)
-                {
-                    RowDefinition step = new RowDefinition();
-                    //step.Height = new GridLength(0, GridUnitType.Auto);
-                    switchGrid.RowDefinitions.Add(step);
+                        
+                    //Generates each row/step
+                    for (int n = 1; n <= rowSteps; n++)
+                    {
+                        RowDefinition step = new RowDefinition();
+                        step.Height = new GridLength(40, GridUnitType.Auto);
+                        switchGrid.RowDefinitions.Add(step);
+
+                        //ON OFF button generation
+                        Button onOff = new Button();
+                        onOff.FontSize = 14;
+                        onOff.VerticalAlignment = VerticalAlignment.Center;
+                        onOff.HorizontalAlignment = HorizontalAlignment.Center;
+                        onOff.Content = false;
+                        //ON OFF button Click + Logic
+                        onOff.Click += (sender, b) =>
+                        {
+                            int r = Grid.GetRow(onOff);
+                            //ON OFF button Logic
+                            //for(int c = 1; c<=inP; c++)
+                            //{
+                            //    if (c != Grid.GetColumn(onOff))
+                            //    {
+                            //        SetButton(switchGrid, r, c, false);
+                            //    }
+                            //}
+                            onOff.Content = !(bool)onOff.Content;
+                        };
+
+                        Grid.SetRow(onOff, n);
+                        Grid.SetColumn(onOff, i - 1);
+
+                        switchGrid.Children.Add(onOff);
+
+                    }
                 }
             }
             else
             {
                 //General grouping lables for multiple inputs
-                for (int sw = 1; sw <= outP; sw++) //for each switch, sw value should not be used to represent a switch
+                for (int chanNum = 1; chanNum <= outP; chanNum++) 
                 {
-                    Grid oSwitch = Load_Grid(1, inP, rowSteps);
-                    Grid.SetColumn(oSwitch, sw-1);
+                    RowDefinition channelLable = new RowDefinition();
+                    channelLable.Height = new GridLength(40, GridUnitType.Auto);
+                    RowDefinition switchOp = new RowDefinition();
+                    switchOp.Height = new GridLength(40, GridUnitType.Auto);
+
+                    switchGrid.RowDefinitions.Add(channelLable);
+                    switchGrid.RowDefinitions.Add(switchOp);
+
+                    Grid oSwitch = Load_Grid(1, inP, rowSteps, chanNum);
+                    Grid.SetColumn(oSwitch, chanNum - 1);
                     Grid.SetRow(oSwitch, 1);
                     switchGrid.Children.Add(oSwitch);
 
                     ColumnDefinition lable = new ColumnDefinition();
-                    lable.Width = new GridLength(0, GridUnitType.Auto);
+                    //lable.Width = new GridLength(40, GridUnitType.Auto);
+
                     switchGrid.ColumnDefinitions.Add(lable);
 
                     TextBlock group = new TextBlock();
-                    group.Text = "Output " + sw;
-                    group.FontSize = 12;
+                    group.Text = "Channel " + chanNum;
+                    group.FontSize = 15;
                     group.FontWeight = FontWeights.Bold;
-                    Grid.SetColumn(group, sw - 1);
+                    Grid.SetColumn(group, chanNum - 1);
                     switchGrid.Children.Add(group);
                 }
             }
             return switchGrid;
         }
+
+
+
+        //Set button does not work
+        private static void SetButton(Grid grid, int row, int column, bool val)
+        {
+            foreach (Button c in grid.Children)
+            {
+                if (Grid.GetRow(c) == row
+                      &&
+                   Grid.GetColumn(c) == column)
+                {
+                    c.Content = val;
+                    break;
+                }
+            }
+        }
+
 
 
         //Load default data grid DataGrid
