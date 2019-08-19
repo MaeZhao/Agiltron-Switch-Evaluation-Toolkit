02b55813edb6ad47162d24f69558a8acb72b3fd9
Mae Zhao <maezhao27@gmail.com>
2019-08-19 19:15:02 -0400

    Consolidating the Frontend Frame

--- SW20190530_Ver3/OpticalSwitchControlSequence.xaml.cs
+++ SW20190530_Ver3/OpticalSwitchControlSequence.xaml.cs
@@ -1056,5 +1056,85 @@ namespace SW20190530_Ver3
 
     #endregion
 
+    #region REGION D (inactive + incomplete): Controls behind the top file/save/edit...etc. bar.
+    partial class OpticalSwitchControlSequence : WindowUIComponents
+    {
 
+        //    //Read test components from file
+        //    private void File_Open_Click(object sender, RoutedEventArgs e)
+        //    {
+        //        OpenFileDialog openFileDialog = new OpenFileDialog();
+        //        if (openFileDialog.ShowDialog() == true)
+        //        {
+        //            Load_Saved_Test(File.ReadAllText(openFileDialog.FileName));
+        //        }
+        //    }
+        //    //Load path to switch logic
+        //    //TODO: move the private fields to just the arguements, get rid of used fields
+        //    private void Load_PTH()
+        //    {
+        //        //String PTH = File.ReadAllText("Logic_PTH/" + type + ".PTH");
+
+        //    }
+        //    //Load saved test file NOT done VERY buggy
+        //    private void Load_Saved_Test(String config)
+        //    {
+        //        StringReader test = new StringReader(config);
+        //        //Loading Test Components
+        //        //TODO: remove console writelines
+        //        int totalSteps = System.Convert.ToInt32(Find_Value(test, '='));
+        //        Console.WriteLine("totalSteps: " + totalSteps);
+
+        //        String title = "";
+        //        if (test.Peek() == 'T')
+        //        {
+        //            title = Find_Value(test, '=');
+        //        }
+        //        Console.WriteLine("title: " + title);
+
+        //        String boardConfig = Find_Value(test, '=');
+        //        Console.WriteLine("boardConfig: " + boardConfig);
+
+        //        //ArrayList stepAr = Read_Step_Array(test, totalSteps);
+
+        //        //IO.DataContext = new SwitchGrid(totalSteps, type, title, stepAr);
+
+        //    }
+
+        //    //Only follows specific step test file format
+        //    //private ArrayList Read_Step_Array(StringReader test, int totalSteps)
+        //    //{
+        //    //    ArrayList stepAr = new ArrayList();
+        //    //    int i = 0;
+        //    //    for (i = 0; test.Peek() != -1; i++)
+        //    //    {
+        //    //        SwitchStep step = new SwitchStep(System.Convert.ToDouble(Find_Value(test, '=')), System.Convert.ToInt32(Find_Value(test, '=')));
+        //    //        Console.WriteLine(i + " time: " + step.Time1);
+        //    //        Console.WriteLine(i + " index: " + step.Index1);
+        //    //        stepAr.Add(step);
+        //    //    }
+        //    //    if (i != totalSteps)
+        //    //    {
+        //    //        MessageBox.Show("Please fix TotalSteps in Test file");
+        //    //    }
+        //    //    return stepAr;
+        //    //}
+
+        //    //Returns the rest of the line skipping everything before (and including) the stop
+        //    private string Find_Value(StringReader test, char stop)
+        //    {
+
+        //        while (test.Peek() != stop && test.Peek() != -1)
+        //        {
+        //            test.Read();
+        //        }
+        //        if (test.Peek() == stop)
+        //        {
+        //            test.Read();
+        //        }
+
+        //        return test.ReadLine();
+        //    }
+    }
+    #endregion
 }
